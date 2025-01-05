0c3e88bf29d2bab7c904281a2222bebbad32503e
jawwad9 <jawwadmuhammad514@gmail.com>
2025-01-05 19:06:22 +0500

    complete auth

--- Anthaction Mongo Db/index.js
+++ Anthaction Mongo Db/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.get("/", (req, res) => {
-    res.send("Hello jawwad!")
+    res.send("Hello World!")
 })
 
 app.use("/api/v1", userrouters);
